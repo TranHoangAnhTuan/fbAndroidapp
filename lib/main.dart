ff06633117eee2a493245c2fe7cde1b0fb33708f
Tuấn <111905166+TranHoangAnhTuan@users.noreply.github.com>
2025-02-19 00:59:50 +0700

    commit the state of bottom nav

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:fb_scrape/pages/posts_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:http/http.dart' as http;
+import 'package:fb_scrape/widgets/animated_nav_item.dart';
 import 'dart:convert';
 import "package:fb_scrape/sign_in/sign_in_screen.dart";
 void main() async {
@@ -41,8 +42,11 @@ class _MainScreenState extends State<MainScreen> {
   final User? user = FirebaseAuth.instance.currentUser;
 
   static final List<Widget> _pages = <Widget>[
-    const ChatPage(),
-    const PostsPage(),
+    const PostsPage(),     // You'll need to create this page
+    const PostsPage(),   // You'll need to create this page
+    const PostsPage(), // You'll need to create this page
+    const PostsPage(), // You'll need to create this page
+    const PostsPage(),  // You'll need to create this page
   ];
 
   void _onItemTapped(int index) {
@@ -117,14 +121,62 @@ class _MainScreenState extends State<MainScreen> {
         ],
       ),
       body: _pages[_selectedIndex], // Display selected page
-      bottomNavigationBar: BottomNavigationBar(
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.chat), label: 'Chat'),
-          BottomNavigationBarItem(icon: Icon(Icons.post_add), label: 'Posts'),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.deepPurple,
-        onTap: _onItemTapped,
+      bottomNavigationBar: Container(
+        height: 80,
+        margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(20),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.2),
+              spreadRadius: 2,
+              blurRadius: 4,
+              offset: const Offset(0, 2),
+            ),
+          ],
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            AnimatedNavItem(
+              icon: 'assets/icons/navigationbar/Home.png',
+              activeIcon: 'assets/icons/navigationbar/transformed/Home.png',
+              label: 'Home',
+              isSelected: _selectedIndex == 0,
+              onTap: () => _onItemTapped(0),
+            ),
+            AnimatedNavItem(
+              icon: 'assets/icons/navigationbar/Search.png',
+              activeIcon: 'assets/icons/navigationbar/transformed/Search.png',
+              label: 'Search',
+              isSelected: _selectedIndex == 1,
+              onTap: () => _onItemTapped(1),
+            ),
+            AnimatedNavItem(
+              icon: 'assets/icons/navigationbar/Bag.png',
+              activeIcon: 'assets/icons/navigationbar/transformed/Bag.png',
+              label: 'Job News',
+              isSelected: _selectedIndex == 2,
+              onTap: () => _onItemTapped(2),
+            ),
+            AnimatedNavItem(
+              icon: 'assets/icons/navigationbar/Setting.png',
+              activeIcon: 'assets/icons/navigationbar/transformed/Setting.png',
+              label: 'Settings',
+              isSelected: _selectedIndex == 3,
+              onTap: () => _onItemTapped(3),
+            ),
+            AnimatedNavItem(
+              icon: 'assets/icons/navigationbar/User.png',
+              activeIcon: 'assets/icons/navigationbar/transformed/User.png',
+              label: 'Profile',
+              isSelected: _selectedIndex == 4,
+              onTap: () => _onItemTapped(4),
+            ),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/widgets/animated_nav_item.dart
@@ -0,0 +1,141 @@
+import 'package:flutter/material.dart';
+
+class AnimatedNavItem extends StatefulWidget {
+  final String icon;
+  final String activeIcon;
+  final String label;
+  final bool isSelected;
+  final VoidCallback onTap;
+
+  const AnimatedNavItem({
+    Key? key,
+    required this.icon,
+    required this.activeIcon,
+    required this.label,
+    required this.isSelected,
+    required this.onTap,
+  }) : super(key: key);
+
+  @override
+  State<AnimatedNavItem> createState() => _AnimatedNavItemState();
+}
+
+class _AnimatedNavItemState extends State<AnimatedNavItem> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _widthAnimation;
+  late Animation<double> _iconSlideAnimation;
+  late Animation<double> _textSlideAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 300),
+      vsync: this,
+    );
+
+    // Modified width animation to expand from center
+    _widthAnimation = Tween<double>(begin: 40, end: 120).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeInOut),
+    );
+
+    // Adjusted icon slide animation
+    _iconSlideAnimation = Tween<double>(begin: 0, end: -30).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeInOut),
+    );
+
+    // Adjusted text slide animation
+    _textSlideAnimation = Tween<double>(begin: 0, end: 30).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeInOut),
+    );
+  }
+
+  @override
+  void didUpdateWidget(AnimatedNavItem oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (widget.isSelected) {
+      _controller.forward();
+    } else {
+      _controller.reverse();
+    }
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _controller,
+      builder: (context, child) {
+        return Stack(
+          alignment: Alignment.center,
+          children: [
+            // Base container for icon (always visible)
+            Container(
+              width: 40,
+              height: 40,
+              alignment: Alignment.center,
+              child: Image.asset(
+                widget.isSelected ? widget.activeIcon : widget.icon,
+                width: 24,
+                height: 24,
+              ),
+            ),
+            // Animated container that expands from center
+            if (widget.isSelected)
+              Container(
+                width: _widthAnimation.value,
+                height: 40,
+                decoration: BoxDecoration(
+                  color: const Color(0xFF46BE5C),
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: Stack(
+                  alignment: Alignment.center,
+                  clipBehavior: Clip.none,
+                  children: [
+                    Transform.translate(
+                      offset: Offset(_iconSlideAnimation.value, 0),
+                      child: Image.asset(
+                        widget.activeIcon,
+                        width: 24,
+                        height: 24,
+                      ),
+                    ),
+                    Transform.translate(
+                      offset: Offset(_textSlideAnimation.value, 0),
+                      child: FadeTransition(
+                        opacity: _controller,
+                        child: Text(
+                          widget.label,
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 14,
+                          ),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            Positioned.fill(
+              child: Material(
+                color: Colors.transparent,
+                child: InkWell(
+                  onTap: widget.onTap,
+                  borderRadius: BorderRadius.circular(8),
+                ),
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
